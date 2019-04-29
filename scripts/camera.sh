72bea51f71fe6bb1cd59bde66173733f6d234aef
Ray Powell <ray@xphoniexx.net>
2019-04-29 18:08:49 -0500

    switched to SRT subttitles, less flaky playback

--- scripts/camera.sh
+++ scripts/camera.sh
@@ -3,26 +3,31 @@
 set -e
 set -u
 
-overlay_text_file='/home/pi/carcam/nmea/loc.vtt'
+overlay_text_file='/home/pi/carcam/nmea/loc'
 VIDEO_BASE="/home/pi/carcam/video"
 
 video_segment="${VIDEO_BASE}/$(date +%F)"
 [ -d $video_segment ] || mkdir -p $video_segment
 
 
-while [ ! -e $overlay_text_file ]; do sleep 1s ; done
+while [ ! -e $overlay_text_file.vtt ]; do sleep 1s ; done
+while [ ! -e $overlay_text_file.srt ]; do sleep 1s ; done
 
 
 raspivid -n -w 720 -h 405 -fps 25 -vf -t 0 -b 1800000 -ih -o - \
-|   ffmpeg -y \
+|   ffmpeg -y -loglevel verbose \
     -i - \
-    -i $overlay_text_file \
+    -i $overlay_text_file.srt \
+    -i $overlay_text_file.vtt \
+    -map 0 -map 1 -map 2 \
+    -c:s copy -metadata:s:s:0 language=eng -metadata:s:s:1 language=ipk \
     -codec:v copy \
     -codec:s copy \
     -f ssegment \
     -segment_time 60 \
     -strftime 1 "${video_segment}/%Y-%m-%d_%H-%M-%S.mkv"
 
+#    -c:s mov_text -metadata:s:s:0 language=eng -metadata:s:s:1 language=ipk \
 #trap "rm stream.m3u8 segments/*.ts" EXIT
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
--- scripts/nmea.sh
+++ scripts/nmea.sh
@@ -10,11 +10,13 @@ set -u
 
 SLEEP_TIME=1 #in seconds
 NMEA_ROOT='/home/pi/carcam/nmea'
-SUBFILE_PREP="${NMEA_ROOT}/.loc.vtt"
-SUBFILE_FINAL="${NMEA_ROOT}/loc.vtt"
+SUBFILE_PREP="${NMEA_ROOT}/.loc"
+SUBFILE_FINAL="${NMEA_ROOT}/loc"
 
-[ -f $SUBFILE_PREP ] && rm $SUBFILE_PREP
-[ -f $SUBFILE_FINAL ] && rm $SUBFILE_FINAL
+[ -f $SUBFILE_PREP.vtt ] && rm $SUBFILE_PREP.vtt
+[ -f $SUBFILE_FINAL.vtt ] && rm $SUBFILE_FINAL.vtt
+[ -f $SUBFILE_PREP.srt ] && rm $SUBFILE_PREP.srt
+[ -f $SUBFILE_FINAL.srt ] && rm $SUBFILE_FINAL.srt
 
 timestamp=$( date +%s )
 while [ "$timestamp" == "" ]
@@ -31,22 +33,39 @@ gpspipe -r -d -l -o $TGTDIR/${datetime}.nmea
 
 last_vtt_timestamp=00
 current_vtt_timestamp="00"
+count=0
 
-echo "WEBVTT" > $SUBFILE_PREP
-echo "" >> $SUBFILE_PREP
+echo "WEBVTT" > $SUBFILE_PREP.vtt
+echo "" >> $SUBFILE_PREP.vtt
+
+#echo "" > $SUBFILE_PREP.srt
+#echo "" >> $SUBFILE_PREP.srt
+touch $SUBFILE_PREP.srt
 
 while [ 1 ]
 do
-        sleep ${SLEEP_TIME}s
+        #sleep ${SLEEP_TIME}s
+	count=$(( count + 1 ))
 	last_vtt_timestamp=${current_vtt_timestamp}
 	current_vtt_timestamp=$(( $(date +%s) - timestamp ))
-
-	echo "00:${last_vtt_timestamp}.000 --> 00:${current_vtt_timestamp}.000" >> $SUBFILE_PREP
-	echo -n "GPS Output:  " >> $SUBFILE_PREP
 	gpspipe -w -n 10 |   grep -m 1 lon | jq . >> $SUBFILE_PREP
-	echo "" >> $SUBFILE_PREP
 
-	cp $SUBFILE_PREP $SUBFILE_FINAL
+	#Classic SRT subtitles
+	echo "$count" >> $SUBFILE_PREP.srt
+	echo "00:00:${last_vtt_timestamp},000 --> 00:00:${current_vtt_timestamp},000" >> $SUBFILE_PREP.srt
+	echo -n "GPS Output:  " >> $SUBFILE_PREP.srt
+	cat $SUBFILE_PREP  >> $SUBFILE_PREP.srt
+	echo "" >> $SUBFILE_PREP.srt
+	cp $SUBFILE_PREP.srt $SUBFILE_FINAL.srt
+
+	#WebVTT subtitles
+	#I get wierd behavior playing these, but left them in anyway
+	echo "00:${last_vtt_timestamp}.000 --> 00:${current_vtt_timestamp}.000" >> $SUBFILE_PREP.vtt
+	echo -n "GPS Output:  " >> $SUBFILE_PREP.vtt
+	cat $SUBFILE_PREP  >> $SUBFILE_PREP.vtt
+	echo "" >> $SUBFILE_PREP.vtt
+	cp $SUBFILE_PREP.vtt $SUBFILE_FINAL.vtt
+
 
 done
 
