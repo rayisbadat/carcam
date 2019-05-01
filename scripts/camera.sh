10ba40b6f5be36feda2c9ff164104ebe4798514b
Ray Powell <ray@xphoniexx.net>
2019-04-30 23:59:36 -0500

    finally got the time sync issues worked out

--- scripts/camera.sh
+++ scripts/camera.sh
@@ -2,32 +2,119 @@
 
 set -e
 set -u
+set -x
 
-overlay_text_file='/home/pi/carcam/nmea/loc'
+DURATION=60 #segment length in seconds
+NMEA_ROOT='/home/pi/carcam/nmea'
+
 VIDEO_BASE="/home/pi/carcam/video"
+DATE="$(date +%F)"
+TS="$(date +%s)"
 
-video_segment="${VIDEO_BASE}/$(date +%F)"
+video_segment="${VIDEO_BASE}/${DATE}-${TS}"
 [ -d $video_segment ] || mkdir -p $video_segment
 
+LOOP_CNT=0
 
-while [ ! -e $overlay_text_file.vtt ]; do sleep 1s ; done
-while [ ! -e $overlay_text_file.srt ]; do sleep 1s ; done
+INIT_SUBTITLES() {
+SUBTITLE=$( mktemp )
+RAW_VIDEO="$( mktemp ).mpeg"
+GPS_LOOP="$( mktemp )"
+GPS_OUT=$( mktemp )
+
+    #[ -f $SUBTITLE.vtt ] && rm $SUBTITLE.vtt
+    #[ -f $SUBTITLE.srt ] && rm $SUBTITLE.srt
+
+    echo "WEBVTT" > $SUBTITLE.vtt
+    echo "" >> $SUBTITLE.vtt
+    touch $SUBTITLE.srt
+    count=0
+    last_vtt_timestamp=0
+    last_vtt_timestamp_min=0
+    current_vtt_timestamp=0
+    current_vtt_timestamp_min=0
+    touch $GPS_LOOP #used so the gps to subtitle files function, knows when the main ffmpeg process ended, so it stops creating subtitles
+    timestamp=$( date +%s )
+
+}
 
 
-raspivid -n -w 720 -h 405 -fps 25 -vf -t 0 -b 1800000 -ih -o - \
-|   ffmpeg -y -loglevel verbose \
-    -i - \
-    -i $overlay_text_file.srt \
-    -i $overlay_text_file.vtt \
+GPS_SUBTITLES() {
+
+while [ -f $GPS_LOOP ]
+do
+
+        count=$(( count + 1 ))
+        last_vtt_timestamp=${current_vtt_timestamp}
+        current_vtt_timestamp="$( printf "%02d" $(( $(date +%s) - timestamp )) )"
+
+        if [ $last_vtt_timestamp -gt 59 ]
+        then
+          last_vtt_timestamp=$(( last_vtt_timestamp - 60 ))
+          last_vtt_timestamp_min="$( printf "%02d" $(( last_vtt_timestamp_min + 1 )) )"
+        fi
+
+        if [ $current_vtt_timestamp -gt 59 ]
+        then
+          current_vtt_timestamp=$(( current_vtt_timestamp - 60 ))
+          current_vtt_timestamp_min="$( printf "%02d" $(( current_vtt_timestamp_min + 1 )) )"
+        fi
+
+        gpspipe -w -n 10 |   grep -m 1 lon  > $GPS_OUT
+
+        #Classic SRT subtitles
+        echo "$count" >> $SUBTITLE.srt
+        echo "00:${last_vtt_timestamp_min}:${last_vtt_timestamp},000 --> 00:${current_vtt_timestamp_min}:${current_vtt_timestamp},000" >> $SUBTITLE.srt
+        cat $GPS_OUT  >> $SUBTITLE.srt
+        echo "" >> $SUBTITLE.srt
+
+        #WebVTT subtitles
+        echo "${last_vtt_timestamp_min}:${last_vtt_timestamp}.000 --> ${current_vtt_timestamp_min}:${current_vtt_timestamp}.000" >> $SUBTITLE.vtt
+	cat $GPS_OUT  >> $SUBTITLE.vtt
+        echo "" >> $SUBTITLE.vtt
+done
+
+
+}
+
+MERGE_SUBS_AND_VID() {
+
+ffmpeg -y
+    -fflags +genpts \
+    -i $RAW_VIDEO \
+    -i $SUBTITLE.srt \
+    -i $SUBTITLE.vtt \
     -map 0 -map 1 -map 2 \
-    -c:s copy -metadata:s:s:0 language=eng -metadata:s:s:1 language=ipk \
     -codec:v copy \
-    -codec:s copy \
-    -f ssegment \
-    -segment_time 60 \
-    -strftime 1 "${video_segment}/%Y-%m-%d_%H-%M-%S.mkv"
+    -c:s copy -metadata:s:s:0 language=eng -metadata:s:s:1 language=ipk \
+    ${video_segment}/${DATE}-${TS}.${LOOP_CNT}.mkv &
 
-#    -c:s mov_text -metadata:s:s:0 language=eng -metadata:s:s:1 language=ipk \
+#    -loglevel verbose \
+#    -map 0 -map 1 -map 2 \
+#    -i $SUBTITLE.srt \
+#    -c:s copy -metadata:s:s:0 language=eng -metadata:s:s:1 language=ipk \
+
+}
+
+
+
+while [ 1 ]
+do
+
+	INIT_SUBTITLES
+	GPS_SUBTITLES &
+	
+	raspivid -n -w 720 -h 405 -fps 25 -vf -t $(( DURATION * 1000 )) -b 1800000 -ih -o $RAW_VIDEO;
+	rm $GPS_LOOP
+
+	MERGE_SUBS_AND_VID &
+
+	#Increment the file name loop counter
+	LOOP_CNT=$(( LOOP_CNT + 1 ))
+
+done
+    
+
 #trap "rm stream.m3u8 segments/*.ts" EXIT
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
--- scripts/nmea.sh
+++ scripts/nmea.sh
@@ -6,23 +6,19 @@
 
 set -e
 set -u
-#set -x
 
-SLEEP_TIME=1 #in seconds
+
 NMEA_ROOT='/home/pi/carcam/nmea'
-SUBFILE_PREP="${NMEA_ROOT}/.loc"
-SUBFILE_FINAL="${NMEA_ROOT}/loc"
+GPS_LOCK=$( /tmp/gpslock )
 
-[ -f $SUBFILE_PREP.vtt ] && rm $SUBFILE_PREP.vtt
-[ -f $SUBFILE_FINAL.vtt ] && rm $SUBFILE_FINAL.vtt
-[ -f $SUBFILE_PREP.srt ] && rm $SUBFILE_PREP.srt
-[ -f $SUBFILE_FINAL.srt ] && rm $SUBFILE_FINAL.srt
+while [ $(ntpq -p| grep GPS  | tr -s " " | cut -d" " -f9 | cut -f2 -d"-" | cut -f1 -d"." ) -gt 5 ]
+do
+	sleep 1
+done
+
+touch $GPS_LOCK
 
 timestamp=$( date +%s )
-while [ "$timestamp" == "" ]
-do
-	timestamp=$( date +%s )
-done
 
 datetime=$(date -d@${timestamp}  +"%Y-%m-%d_%H:%M:%S")
 date=$( date -d@${timestamp}  +"%Y-%m-%d" )
@@ -30,42 +26,3 @@ TGTDIR="${NMEA_ROOT}/${date}"
 
 [ -d "$TGTDIR" ]  || mkdir -p $TGTDIR
 gpspipe -r -d -l -o $TGTDIR/${datetime}.nmea
-
-last_vtt_timestamp=00
-current_vtt_timestamp="00"
-count=0
-
-echo "WEBVTT" > $SUBFILE_PREP.vtt
-echo "" >> $SUBFILE_PREP.vtt
-
-#echo "" > $SUBFILE_PREP.srt
-#echo "" >> $SUBFILE_PREP.srt
-touch $SUBFILE_PREP.srt
-
-while [ 1 ]
-do
-        #sleep ${SLEEP_TIME}s
-	count=$(( count + 1 ))
-	last_vtt_timestamp=${current_vtt_timestamp}
-	current_vtt_timestamp=$(( $(date +%s) - timestamp ))
-	gpspipe -w -n 10 |   grep -m 1 lon | jq . >> $SUBFILE_PREP
-
-	#Classic SRT subtitles
-	echo "$count" >> $SUBFILE_PREP.srt
-	echo "00:00:${last_vtt_timestamp},000 --> 00:00:${current_vtt_timestamp},000" >> $SUBFILE_PREP.srt
-	echo -n "GPS Output:  " >> $SUBFILE_PREP.srt
-	cat $SUBFILE_PREP  >> $SUBFILE_PREP.srt
-	echo "" >> $SUBFILE_PREP.srt
-	cp $SUBFILE_PREP.srt $SUBFILE_FINAL.srt
-
-	#WebVTT subtitles
-	#I get wierd behavior playing these, but left them in anyway
-	echo "00:${last_vtt_timestamp}.000 --> 00:${current_vtt_timestamp}.000" >> $SUBFILE_PREP.vtt
-	echo -n "GPS Output:  " >> $SUBFILE_PREP.vtt
-	cat $SUBFILE_PREP  >> $SUBFILE_PREP.vtt
-	echo "" >> $SUBFILE_PREP.vtt
-	cp $SUBFILE_PREP.vtt $SUBFILE_FINAL.vtt
-
-
-done
-
